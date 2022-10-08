6ad8d7068d5d67444afac5eac347ae85b9a325d2
Hyemin Oh <52392424+hyeminoh96@users.noreply.github.com>
2022-10-08 22:38:26 +0900

    create post methods for buyer, seller, property

--- main.py
+++ main.py
@@ -6,3 +6,33 @@ app = FastAPI()
 @app.get("/")
 def say_hello():
     return {"message": "Hello World"}
+
+
+@app.get("/buyers")
+def read_buyers():
+    return "all buyers"
+
+
+@app.get("/buyers/{buyer_id}")
+def read_buyer(buyer_id: int = None):
+    return {"buyer_id": buyer_id}
+
+
+@app.get("/sellers")
+def read_sellers():
+    return "all sellers"
+
+
+@app.get("/sellers/{seller_id}")
+def read_seller(seller_id: int = None):
+    return {"seller_id": seller_id}
+
+
+@app.get("/properties")
+def read_properties():
+    return "all properties"
+
+
+@app.get("/properties/{property_id}")
+def read_property(property_id: int = None):
+    return {"property_id": property_id}

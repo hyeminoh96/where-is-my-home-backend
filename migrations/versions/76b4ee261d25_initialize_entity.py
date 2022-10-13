1a05c4b7d6aecf2a7ebc4b7b4995b6a457a58e97
Hyemin Oh <52392424+hyeminoh96@users.noreply.github.com>
2022-10-13 22:32:36 +0900

    generate init migration

---
+++ migrations/versions/76b4ee261d25_initialize_entity.py
@@ -0,0 +1,24 @@
+"""initialize entity
+
+Revision ID: 76b4ee261d25
+Revises: 
+Create Date: 2022-10-13 22:26:29.269882
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '76b4ee261d25'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass

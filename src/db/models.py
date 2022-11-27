31c823c37c8301e86e124acb45266ec8f00aea55
Hyemin Oh <52392424+hyeminoh96@users.noreply.github.com>
2022-11-27 18:37:42 +0900

    set string lengths

--- src/db/models.py
+++ src/db/models.py
@@ -1,36 +1,36 @@
 from sqlalchemy import Column, Integer, String, Boolean
 
-from database import Base
+from src.db.database import Base
 
 
 class Buyer(Base):
     __tablename__ = "buyer"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String)
-    phone_number = Column(String)
-    address = Column(String)
+    name = Column(String(20))
+    phone_number = Column(String(20))
+    address = Column(String(50))
     is_active = Column(Boolean, default=True)
-    description = Column(String)
+    description = Column(String(100))
 
 
 class Seller(Base):
     __tablename__ = "seller"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String)
-    phone_number = Column(String)
-    address = Column(String)
+    name = Column(String(20))
+    phone_number = Column(String(20))
+    address = Column(String(50))
     is_active = Column(Boolean, default=True)
-    description = Column(String)
+    description = Column(String(100))
 
 
 class Property(Base):
     __tablename__ = "property"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    address = Column(String)
-    type = Column(String)  # FIXME to enum
+    address = Column(String(50))
+    type = Column(String(20))  # FIXME to enum
     is_active = Column(Boolean, default=True)
-    description = Column(String)
+    description = Column(String(100))
     # TODO: owner_id relation 설정

b4cce4238a890c3933675f32f53782a79d36f6be
Hyemin Oh <52392424+hyeminoh96@users.noreply.github.com>
2022-11-29 21:50:42 +0900

    migration 생성 테스트

---
+++ migrations/versions/8a2035f208f7_modify_column_name.py
@@ -0,0 +1,25 @@
+"""modify column name
+
+
+Revision ID: 8a2035f208f7
+Revises: 76b4ee261d25
+Create Date: 2022-11-29 21:41:47.289498
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8a2035f208f7'
+down_revision = '76b4ee261d25'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
